0d2764a90190c68175bdd5fa82593adb5d6faed0
Jasper Samsom <s1187996@student.windesheim.nl>
2025-04-14 12:57:52 +0200

    Opdracht 2

--- opdrachten/010_variabelen/opdr_1/opdr_1.py
+++ opdrachten/010_variabelen/opdr_1/opdr_1.py
@@ -1,8 +1,12 @@
 # Opdracht 1
-# Naam student:
-# Groep:
+# Naam student: Jasper Samsom
+# Groep: IT2A
 
 # Hier komt je code...
 
 my_list = (13, "wat een weertje, 12.5, 8")
 print(my_list)
+
+#git init
+#git branch -M main
+#git push -u origin main
